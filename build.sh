047cdbe6d723c646d76a01d0b08a9572fbc431a2
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2021-01-19 00:11:48 -0300

    insert checkout

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ if [ $github_version != $ftp_version ]
 then
   git clone https://github.com/containers/conmon.git
   cd conmon
+  git checkout v$github_version
   make
   cd bin
   ./conmon --version
   mv conmon conmon-$github_version
+  ls
 
   lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/conmon/latest/conmon-$ftp_version"
   lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/conmon/latest conmon-$github_version"
