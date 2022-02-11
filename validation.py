47bc5c5ecc3aabb10102cbf75f32709ef0a254d7
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2022-02-11 11:10:39 -0300

    Create validation.py

---
+++ validation.py
@@ -0,0 +1,19 @@
+file = open('github_version.txt', 'r')
+version = file.readline()
+version = version.strip()
+file.close()
+
+file = open('github_version.txt', 'w')
+file.writelines(version)
+file.close()
+
+file = open('ftp_version.txt', 'r')
+version = file.readline()
+version = version.strip()
+version = version.rstrip('/')
+version = version.rstrip('<')
+file.close()
+
+file = open('ftp_version.txt', 'w')
+file.writelines(version)
+file.close()
